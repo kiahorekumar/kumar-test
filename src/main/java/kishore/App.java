6613d7c04f90c57d25c88d6fc5593ccf5ba2a166
kiashorekumar <kishorekumaryadav251@gmail.com>
2018-09-12 21:14:57 +0530

    third commit

--- src/main/java/kishore/App.java
+++ src/main/java/kishore/App.java
@@ -4,6 +4,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hai kishore hello buddy");
-        System.out.println( "second commit");    
+        System.out.println( "third commit");    
     }
 }
