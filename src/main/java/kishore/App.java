a177f3ad94fe519a1743c381b9dbf78cc2204b17
kiashorekumar <kishorekumaryadav251@gmail.com>
2018-09-12 21:01:33 +0530

    first commit

--- src/main/java/kishore/App.java
+++ src/main/java/kishore/App.java
@@ -3,6 +3,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Hai kishore hello buddy" );
+        System.out.println( "Hai kishore hello buddy");
+        System.out.println( "first commit");    
     }
 }
